dda65b2a91dc6244148aae4c2d1a26a6d3766a78
Prantik Medhi <140103052+prantikmedhi@users.noreply.github.com>
2025-05-30 17:28:09 +0530

    Update page.tsx

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -108,32 +108,44 @@ export default function ServicesPage() {
             width={600}
             height={140}
             rotate={12}
-            gradient="from-white/[0.06]"
+            gradient="from-white/[0.08]"
             className="left-[-10%] md:left-[-5%] top-[15%] md:top-[20%]"
           />
+
           <ElegantShape
             delay={0.5}
             width={500}
             height={120}
             rotate={-15}
-            gradient="from-white/[0.06]"
+            gradient="from-white/[0.08]"
             className="right-[-5%] md:right-[0%] top-[70%] md:top-[75%]"
           />
+
           <ElegantShape
             delay={0.4}
             width={300}
             height={80}
             rotate={-8}
-            gradient="from-white/[0.06]"
+            gradient="from-white/[0.08]"
             className="left-[5%] md:left-[10%] bottom-[5%] md:bottom-[10%]"
           />
+
           <ElegantShape
             delay={0.6}
             width={200}
             height={60}
             rotate={20}
-            gradient="from-white/[0.06]"
+            gradient="from-white/[0.08]"
             className="right-[15%] md:right-[20%] top-[10%] md:top-[15%]"
+          />
+
+          <ElegantShape
+            delay={0.7}
+            width={150}
+            height={40}
+            rotate={-25}
+            gradient="from-white/[0.08]"
+            className="left-[20%] md:left-[25%] top-[5%] md:top-[10%]"
           />
         </div>
 
